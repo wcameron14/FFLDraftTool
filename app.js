07f808e49069f7ff98dff61ae2cacdf1c05de39c
Wade Cameron <wcameron@singlestoneconsulting.com>
2018-07-27 16:45:40 -0400

    Add ejs, app, index.ejs, header and footer

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ var express = require ('express');
 var mysql = require ('mysql');
 var nodemon = require('nodemon');
 var validator = require('express-validator');
+var path = require('path');
 
 var app = express();
 
+//View Engine
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+//Set Static Path
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(req, res){
-    res.send('Hello World');
+    res.render('index', {
+        title:'Fantasy Football Draft Tool'
+    });
 });
 
 app.listen(3000,function(){
