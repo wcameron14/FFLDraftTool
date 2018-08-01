859b40352e817720eb3b6dfbed93a212871977aa
Wade Cameron <wcameron@singlestoneconsulting.com>
2018-08-01 15:46:00 -0400

    Update

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var mysql = require ('mysql');
 var nodemon = require('nodemon');
 var validator = require('express-validator');
 var path = require('path');
+var async = require('async');
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "F4nt4$yF00tb4ll!",
+    database: "FFL_league_data",
+    port: "3000"
+});
 
 var app = express();
 
@@ -20,6 +28,18 @@ app.get('/', function(req, res){
     });
 });
 
+
+
 app.listen(3000,function(){
     console.log('Server Started on Port 3000...');
-})
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected to mysql")
+    var sql = "Create Table players (displayname VARCHAR(255), position VARCHAR(20),team VARCHAR(255), jersey INT(3))";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+    });
+});
